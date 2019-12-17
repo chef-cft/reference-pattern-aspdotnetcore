db15a4928173c6e98539ae4b0f945726518d453c
niamhcahill <ncahill@chef.io>
2019-12-16 18:46:02 -0800

    Update bootstrap_hab PowerShell script to reflect changes to Habitat Windows Service

--- terraform/aws/templates/bootstrap_hab.ps1
+++ terraform/aws/templates/bootstrap_hab.ps1
@@ -11,12 +11,11 @@ do{
 }
 until($LASTEXITCODE -eq 0)
 
-hab pkg exec core/windows-service install
 New-NetFirewallRule -DisplayName 'Habitat TCP' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 9631,9638
 New-NetFirewallRule -DisplayName 'Habitat UDP' -Direction Inbound -Action Allow -Protocol UDP -LocalPort 9638
 
 $svcPath = Join-Path $env:SystemDrive "hab\svc\windows-service"
 [xml]$configXml = Get-Content (Join-Path $svcPath HabService.dll.config)
 $configXml.configuration.log4net.appender.file.value
-$configXml.configuration.appSettings.add[2].value = "--no-color --peer=${peer_ip}"
+$configXml.configuration.appSettings.add[1].value = "--no-color --peer=${peer_ip}"
 $configXml.Save((Join-Path $svcPath HabService.dll.config))
